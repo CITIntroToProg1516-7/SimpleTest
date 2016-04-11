2b906f6c5d4ef92788c506c91536b793b8aaadde
cmneville <cmneville@180CMNEVILLE.hcps.internal>
2016-04-11 10:53:46 -0400

    made basic stuff

--- src/test/java/cit/programming/simpletest/MainTestTest.java
+++ src/test/java/cit/programming/simpletest/MainTestTest.java
@@ -5,14 +5,18 @@
  */
 package cit.programming.simpletest;
 
-import org.junit.Test;
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+import org.junit.After;
 import static org.junit.Assert.*;
+import org.junit.Before;
 
 /**
  *
  * @author cmneville
  */
 public class MainTestTest {
+    private final ByteArrayOutputStream out = new ByteArrayOutputStream();
     
     public MainTestTest() {
     }
@@ -20,12 +24,24 @@ public class MainTestTest {
     /**
      * Test of main method, of class MainTest.
      */
+    @Before
+    public void setStream(){
+        System.setOut(new PrintStream(out));
+    }
+    
+    @After
+    public void cleanStream(){
+        System.setOut(null);
+    }
+    
+  
+    
     @org.junit.Test
     public void testMain() {
         System.out.println("main");
         String[] args = null;
         MainTest.main(args);
-        assertEquals(1,1);
+        assertEquals("hello world", out.toString());
         // TODO review the generated test code and remove the default call to fail.
         //fail("The test case is a prototype.");
     }
